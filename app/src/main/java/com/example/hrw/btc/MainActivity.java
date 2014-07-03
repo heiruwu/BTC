70fbe274de75bc962c86693c97ed6f1c18cc2e1b
gt212345 <wuheiru.5203@gmail.com>
2014-07-03 09:20:23 +0800

    Build bluetooth device select menu

--- app/src/main/java/com/example/hrw/btc/MainActivity.java
+++ app/src/main/java/com/example/hrw/btc/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hrw.btc;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -8,6 +12,7 @@ import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,8 +20,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity
@@ -107,11 +120,104 @@ public class MainActivity extends ActionBarActivity
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        TextView btStat,rcMessage;
+        BluetoothAdapter mBluetoothAdapter;
+        BluetoothDevice mDevice;
+        ListView mbtDevices;
+        BluetoothSocket mBluetoothSocket;
+        InputStream mInputStream;
+        String selectDevice;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        }
+
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            btStat = (TextView)getView().findViewById(R.id.btStat);
+            rcMessage = (TextView)getView().findViewById(R.id.rcMessage);
+            mbtDevices = (ListView)getView().findViewById(R.id.btDevices);
+            mbtDevices = (ListView)getView().findViewById(R.id.btDevices);
+            if (mBluetoothAdapter == null) {
+                Toast.makeText(getActivity(), "not fucking support",
+                        Toast.LENGTH_SHORT).show();
+            }
+            if (!mBluetoothAdapter.enable()) {
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Intent turnOnIntent = new Intent(
+                            BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(turnOnIntent, 0);
+                }
+
+            }
+            btStat.setText("Paired Devices");
+            rcMessage.append("Searching paired device......\n");
+            findBT();
+        }
+
+        void findBT() {
+            int mDeviceCount = 0;
+            final Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
+                    .getBondedDevices();
+            final String[] mDevices = new String[pairedDevices.size()];
+            if (pairedDevices.size() > 0) {
+                rcMessage.append("Devices found:\n");
+                for (BluetoothDevice device : pairedDevices) {
+                    mDevices[mDeviceCount] = device.getName();
+                    mDeviceCount++;
+                }
+                for (int i = 0;i<mDevices.length;i++){
+                    rcMessage.append(mDevices[i]+"\n");
+                }
+                mbtDevices.setAdapter(new ArrayAdapter<String>(
+                    getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    mDevices
+                    ));
+                mbtDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectDevice = ((TextView) view).getText().toString();
+                        rcMessage.append(selectDevice+" selected\n");
+                        for (BluetoothDevice device : pairedDevices){
+                            if(device.getName().equals(selectDevice)){
+                                mDevice = device;
+                                openBT();
+                                break;
+                            }
+                        }
+                    }
+                });
+            }
+            rcMessage.append("Waiting for selection\n");
+        }
+        void openBT(){
+            rcMessage.append("Connecting......\n");
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // Standard
+            // SerialPortService
+            // ID
+            try {
+                mBluetoothSocket = mDevice.createRfcommSocketToServiceRecord(uuid);
+                mBluetoothSocket.connect();
+                rcMessage.append("Device connected\n");
+                mInputStream = mBluetoothSocket.getInputStream();
+                rcMessage.append("InputStream initialized\nwaiting for input\n");
+            }catch(IOException e){
+                rcMessage.append("Connect error:\nDevices not responding\n");
+            }
+//          beginListenForData();
+        }
+
+
 
         /**
          * Returns a new instance of this fragment for the given section
