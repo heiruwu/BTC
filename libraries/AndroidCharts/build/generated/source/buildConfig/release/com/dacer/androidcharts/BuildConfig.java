b813de41e2818dc13eab3ebbb9d726d3f3fa0cf1
Heiru Wu <wuheiru.5203@gmail.com>
2014-08-01 13:43:37 +0800

    some changes

---
+++ libraries/AndroidCharts/build/generated/source/buildConfig/release/com/dacer/androidcharts/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dacer.androidcharts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.dacer.androidcharts";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
